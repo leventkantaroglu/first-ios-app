31af58cabb829941e34497b7dbad5daf35224373
Levent Kantaroğlu <leventkantaroglu@dusyeri.com.tr>
2021-11-27 16:33:18 +0300

    Initial Commit

---
+++ first_ios_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  first_ios_app
+//
+//  Created by Admin on 27.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
